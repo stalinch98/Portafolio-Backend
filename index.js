337fddeea2d4a70add55f9d99ec1519e2b85522e
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-09-16 00:45:43 -0500

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { config } = require('./config/config');
 const emailApi = require('./routes/email-route');
 
 app.use(express.json());
+app.use(cors());
 emailApi(app);
 
 app.listen(config.port, () =>
